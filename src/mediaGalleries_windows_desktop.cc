d4799ac60d684467e236f4817d476dc86bd701e1
Benjamin P. Jung <headcr4sh@gmail.com>
2015-05-05 21:13:07 +0200

    Add exception handling code.

--- src/mediaGalleries_windows_desktop.cc
+++ src/mediaGalleries_windows_desktop.cc
@@ -13,6 +13,17 @@
 
 #include "mediaGalleries.h"
 
+/**
+ * Throws the given value, if it is not <code>S_OK</code>.
+ * @param res
+ *   HRESULT to be checked.
+ */
+void _(HRESULT res) {
+  if (S_OK != res) {
+    throw res;
+  }
+}
+
 using std::wprintf;
 using namespace v8;
 
@@ -32,16 +43,16 @@ Local<Array> getPictureGalleries(Isolate* isolate) {
     HRESULT res = S_OK;
 
     IShellLibrary *picturesLibrary;
-    res = SHLoadLibraryFromKnownFolder(FOLDERID_PicturesLibrary,
+    _(SHLoadLibraryFromKnownFolder(FOLDERID_PicturesLibrary,
                                        STGM_READ,
-                                       IID_PPV_ARGS(&picturesLibrary));
+                                       IID_PPV_ARGS(&picturesLibrary)));
 
     IShellItemArray *pictureFolders;
-    picturesLibrary->GetFolders(LFF_FORCEFILESYSTEM, IID_PPV_ARGS(&pictureFolders));
+    _(picturesLibrary->GetFolders(LFF_FORCEFILESYSTEM, IID_PPV_ARGS(&pictureFolders)));
 
 
     DWORD pictureFoldersCount;
-    pictureFolders->GetCount(&pictureFoldersCount);
+    _(pictureFolders->GetCount(&pictureFoldersCount));
 
     Local<Array> galleries = Array::New(isolate, (int) pictureFoldersCount);
 
@@ -52,13 +63,13 @@ Local<Array> getPictureGalleries(Isolate* isolate) {
         LPWSTR path;
         char *path_c;
 
-        pictureFolders->GetItemAt(i, &pictureFolder);
+        _(pictureFolders->GetItemAt(i, &pictureFolder));
 
-        pictureFolder->GetDisplayName(SIGDN_NORMALDISPLAY, &name);
+        _(pictureFolder->GetDisplayName(SIGDN_NORMALDISPLAY, &name));
         name_c = new char[wcslen(name) + 1];
         wsprintfA(name_c, "%S", name);
 
-        pictureFolder->GetDisplayName(SIGDN_FILESYSPATH, &path);
+        _(pictureFolder->GetDisplayName(SIGDN_FILESYSPATH, &path));
         path_c = new char[wcslen(path) + 1];
         wsprintfA(path_c, "%S", path);
 
